c667f8771a198cd78985e0c3f430017d1de3305d
bagusgandhi <bagusgandhi4@gmail.com>
2022-10-26 11:50:57 +0700

    fixed env issue

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
 
-dotenv.config({ path: './.env' });
 const app = require('./app');
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 
 const DB = process.env.MONGO_URI.replace(
     '<PASSWORD>',
